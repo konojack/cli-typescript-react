8743850098d3b2ee9ec14369afd1404dfc923053
konojack <konojack@gmail.com>
2022-04-27 19:04:28 +0200

    Transpiling JSX code inside browser by esbuild startService() and transform()

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,54 @@
-import ReactDOM from 'react-dom';
+import { useEffect, useState, useRef } from 'react';
+import ReactDOM from 'react-dom/client';
+import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
-  return <div>Hello World</div>;
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClickHandler = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        name="codeInput"
+        id="codeInput"
+        cols={30}
+        rows={10}
+      ></textarea>
+      <div>
+        <button onClick={onClickHandler}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root')!;
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
