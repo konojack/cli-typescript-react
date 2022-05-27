afbf2adfa814b59ee59c18df03824736a92940a4
konojack <konojack@gmail.com>
2022-05-27 16:44:27 +0200

    Adding some styles to FORMAT button

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import './code-editor.css';
 import MonacoEditor, { OnMount, OnChange } from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
@@ -27,21 +28,28 @@ const CodeEditor: React.FC<CodeEditorProps> = ({
     const unformattedCode = editorRef.current?.getModel()?.getValue();
 
     // format code
-    const formattedCode = prettier.format(unformattedCode, {
-      parser: 'babel',
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      singleQuote: true,
-    });
+    const formattedCode = prettier
+      .format(unformattedCode, {
+        parser: 'babel',
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, '');
 
     // set formatted code
     editorRef.current?.setValue(formattedCode);
   };
 
   return (
-    <div>
-      <button onClick={onFormatClick}>Fromat</button>
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-smaill"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         onChange={onEditorChange}
         onMount={onEditorDidMount}
