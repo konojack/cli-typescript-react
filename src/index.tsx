412b58b6d74ab2deb4deba49c1a0f854aba24712
konojack <konojack@gmail.com>
2022-05-28 09:29:05 +0200

    Extracting bundling logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,67 +1,19 @@
-import { useEffect, useState, useRef } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
+import bundle from './bundler';
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
   const [input, setInput] = useState<string | undefined>('');
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
+  const [code, setCode] = useState<string>('');
 
   const onClickHandler = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    const output = await bundle(input);
+    setCode(output);
   };
-
-  const html = `
-    <html>
-     <head></head>
-     <body>
-      <div id="root"></div>
-      <script>
-        window.addEventListener('message', (event) => {
-          try {
-            eval(event.data);
-          } catch (err) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>';
-            throw err;
-          }
-        }, false);
-      </script>
-     </body>
-    </html>
-  `;
 
   return (
     <div>
@@ -69,18 +21,10 @@ const App = () => {
         initialValue="const a = 1;"
         onChangeEditor={value => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={e => setInput(e.target.value)}
-        name="codeInput"
-        id="codeInput"
-        cols={30}
-        rows={10}
-      ></textarea>
       <div>
         <button onClick={onClickHandler}>Submit</button>
       </div>
-      <iframe ref={iframe} srcDoc={html} title="elo" sandbox="allow-scripts" />
+      <Preview code={code} />
     </div>
   );
 };
