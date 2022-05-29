9a308d7012f1c189b934701d15eb1249c90cc80c
konojack <konojack@gmail.com>
2022-05-29 20:29:07 +0200

    Adding resizing horizontally

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -16,12 +16,14 @@ const CodeCell = () => {
   };
 
   return (
-    <Resizable direction="horizontal">
+    <Resizable direction="vertical">
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-        <CodeEditor
-          initialValue="const a = 1;"
-          onChangeEditor={value => setInput(value)}
-        />
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChangeEditor={value => setInput(value)}
+          />
+        </Resizable>
         <Preview code={code} />
       </div>
     </Resizable>
--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,5 +1,5 @@
 import './resizable.css';
-import { ResizableBox } from 'react-resizable';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import React from 'react';
 
 interface ResizableProps {
@@ -8,11 +8,28 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox width={Infinity} height={300} resizeHandles={['s']}>
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === 'horizontal') {
+    resizableProps = {
+      className: 'resize-horizontal',
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      width: window.innerWidth * 0.75,
+      height: Infinity,
+      resizeHandles: ['e'],
+    };
+  } else {
+    resizableProps = {
+      minConstraints: [Infinity, 50],
+      maxConstraints: [Infinity, window.innerHeight * 0.98],
+      width: Infinity,
+      height: 300,
+      resizeHandles: ['s'],
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
