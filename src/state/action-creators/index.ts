ced6e1186e1f12af64c6e7cdb22c2ebe2dfe0ef5
konojack <konojack@gmail.com>
2022-06-14 14:49:39 +0200

    Creating Action Creators

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,1 +1,53 @@
-export {};
+import { ActionType } from '../action-types';
+import {
+  Action,
+  MoveCellAction,
+  DeleteCellAction,
+  UpdateCellAction,
+  InsertCellBeforeAction,
+  DirectionType,
+} from '../actions';
+import { CellTypes } from '../cell';
+
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content,
+    },
+  };
+};
+
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
+
+export const moveCell = (
+  id: string,
+  direction: DirectionType
+): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
+
+export const insertBeforeCell = (
+  id: string,
+  cellType: CellTypes
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type: cellType,
+    },
+  };
+};
