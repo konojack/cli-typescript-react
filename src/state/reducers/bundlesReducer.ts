1caeed3bd996da703ef0347dcea9b6579380a593
konojack <konojack@gmail.com>
2022-07-13 16:24:58 +0200

    Creating reducer for bundles

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -1,1 +1,28 @@
-export {};
+import produce from 'immer';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+interface BundlesState {
+  [key: string]: {
+    code: string;
+    err: string;
+  };
+}
+
+const initialState: BundlesState = {};
+
+const reducer = (
+  state: BundlesState = initialState,
+  action: Action
+): BundlesState => {
+  return produce(state, draft => {
+    switch (action.type) {
+      case ActionType.BUNDLE_CREATED:
+        return draft;
+      default:
+        return draft;
+    }
+  });
+};
+
+export default reducer;
